b40b129c55e0d3feb67601a1b6202275b4748040
jeremynac <jeremynac@hotmail.fr>
2021-11-21 18:59:49 +0100

    Add humidity

--- temp-and-humidity/src/App.js
+++ temp-and-humidity/src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Page } from "./components/Page";
+import './App.css'
+import { Page } from './components/Page'
+import React from 'react'
+require('dotenv').config()
 
-function App() {
+function App () {
   return (
     <div className="App">
       <Page />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
